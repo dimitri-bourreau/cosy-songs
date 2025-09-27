86f7bdba3e00fd441ac24d23cba65d3b3ad5b18b
Dimitri Bourreau <dimitri@release-dev.com>
2025-09-28 00:06:37 +0200

    🔥 PROJECT - Remove v1 files

--- features/from-song-string-to-array.feat.ts
+++
@@ -1,9 +0,0 @@
-export const fromSongsStringToArray = (songs: string): string[] => {
-  return songs
-    .split('\n')
-    .filter(song => song.match(/\s*-?(\w+)\s*-\s*(\w+)/))
-    .map(song => {
-      if (song.match(/^- (.*)$/)) return song.slice(2).trim();
-      return song.trim()
-    })
-}
--- features/get-episodes-from-url.feat.ts
+++
@@ -1,8 +0,0 @@
-import {parse} from "https://deno.land/x/xml/mod.ts";
-import {Episode} from "../types/episode.type.ts";
-
-export const getEpisodesFromURL = async (url: string): Promise<Episode[]> => {
-  const fetchResponse = await fetch(url);
-  const xmlContent = await fetchResponse.text();
-  return parse(xmlContent)?.rss?.channel?.item as Episode[];
-}
